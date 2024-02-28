18ad07cd8b3a8df253dd27d85cdac64d4214c85a
Nikita Iakovlev <nikitayakovlev548@gmail.com>
2024-02-28 18:51:37 +0300

    Создал класс для работы на странице с товарами

---
+++ pages/product_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+
+    def add_to_cart(self):
+        basket_button = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
+        basket_button.click()
+
+    def guest_should_see_added_to_cart_message(self):
+        assert self.is_element_present(*ProductPageLocators.BASKET_NOTIFICATION), "Added to cart message is not presented"
+
+    def guest_should_see_price_message(self):
+        assert self.is_element_present(*ProductPageLocators.PRICE_NOTIFICATION), "Price message is not presented"
+
+    def price_in_message_should_be_equal_to_product_price(self):
+        assert self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text == self.browser.find_element(*ProductPageLocators.PRICE_NOTIFICATION).text, "Prices are different"
+
+    def title_in_added_to_cart_message_should_be_equal_product_title(self):
+        assert self.browser.find_element(*ProductPageLocators.BASKET_NOTIFICATION).text == self.browser.find_element(*ProductPageLocators.BOOK_TITLE).text, "Titles are different"
