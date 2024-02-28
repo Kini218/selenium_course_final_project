cf3c1ee8eb5dfc509a6f2442d7b22d759c6285d4
Nikita Iakovlev <nikitayakovlev548@gmail.com>
2024-02-28 18:48:46 +0300

    Добавил новый класс ProductPageLocators с селекторами

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form")
+    BASKET_NOTIFICATION = (By.CSS_SELECTOR, ".alert.alert-safe.alert-noicon.alert-success.fade.in strong")
+    PRICE_NOTIFICATION = (By.CSS_SELECTOR, ".alert.alert-safe.alert-noicon.alert-info.fade.in .alertinner strong")
+    BOOK_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main .price_color")
+    BOOK_TITLE = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
