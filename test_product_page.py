ab0d386398314ecfce2c1c45765477ff15c214ac
Nikita Iakovlev <nikitayakovlev548@gmail.com>
2024-03-04 12:05:26 +0300

    Добавил 3 теста, для проверки сообщения об успешном добавлениии товара.

--- test_product_page.py
+++ test_product_page.py
@@ -21,3 +21,25 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.guest_should_see_price_message()
     page.price_in_message_should_be_equal_to_product_price()
     page.title_in_added_to_basket_message_should_be_equal_product_title()
+
+@pytest.mark.xfail(reason = "guest should not see success message")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail(reason = "test message should not disappeared")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.success_message_should_not_disappeared()
