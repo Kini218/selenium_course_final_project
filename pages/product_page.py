92bd1a11edd20317d3b1cfd2f94a030eb35acb24
Nikita Iakovlev <nikitayakovlev548@gmail.com>
2024-03-04 10:10:48 +0300

    Заменил все "cart" на "basket", чтобы не было путаницы

--- pages/product_page.py
+++ pages/product_page.py
@@ -3,12 +3,12 @@ from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
 
-    def add_to_cart(self):
+    def add_to_basket(self):
         basket_button = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
         basket_button.click()
 
-    def guest_should_see_added_to_cart_message(self):
-        assert self.is_element_present(*ProductPageLocators.BASKET_NOTIFICATION), "Added to cart message is not presented"
+    def guest_should_see_added_to_basket_message(self):
+        assert self.is_element_present(*ProductPageLocators.BASKET_NOTIFICATION), "Added to basket message is not presented"
 
     def guest_should_see_price_message(self):
         assert self.is_element_present(*ProductPageLocators.PRICE_NOTIFICATION), "Price message is not presented"
@@ -16,5 +16,5 @@ class ProductPage(BasePage):
     def price_in_message_should_be_equal_to_product_price(self):
         assert self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text == self.browser.find_element(*ProductPageLocators.PRICE_NOTIFICATION).text, "Prices are different"
 
-    def title_in_added_to_cart_message_should_be_equal_product_title(self):
+    def title_in_added_to_basket_message_should_be_equal_product_title(self):
         assert self.browser.find_element(*ProductPageLocators.BASKET_NOTIFICATION).text == self.browser.find_element(*ProductPageLocators.BOOK_TITLE).text, "Titles are different"
--- test_product_page.py
+++ test_product_page.py
@@ -4,9 +4,9 @@ def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
-    page.add_to_cart()
+    page.add_to_basket()
     page.solve_quiz_and_get_code()
-    page.guest_should_see_added_to_cart_message()
+    page.guest_should_see_added_to_basket_message()
     page.guest_should_see_price_message()
     page.price_in_message_should_be_equal_to_product_price()
-    page.title_in_added_to_cart_message_should_be_equal_product_title()
+    page.title_in_added_to_basket_message_should_be_equal_product_title()
