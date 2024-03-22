d645e5bb25aa094b9034308bba1147c5f8604537
Nikita Iakovlev <nikitayakovlev548@gmail.com>
2024-03-22 13:08:36 +0300

    Доавил новые локаторы для страницы регистрации

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,7 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini.pull-right.hidden-xs span a")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class BasketPageLocators():
     PRODUCT_IN_BASKET = (By.CSS_SELECTOR, "#basket_formset")
@@ -15,6 +16,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTER_FORM_EMAIL = (By.CSS_SELECTOR, "[name= 'registration-email']")
+    REGISTER_FORM_PASSWORD = (By.CSS_SELECTOR, "[name= 'registration-password1']")
+    REGISTER_FORM_REPEATED_PASSWORD = (By.CSS_SELECTOR, "[name= 'registration-password2']")
+    REGISTER_FORM_SUBMIT_BUTTON = (By.CSS_SELECTOR, "[name = 'registration_submit']")
 
 class ProductPageLocators():
     BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form")
